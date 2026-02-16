6e963dad4368c4d17931a017c90f922576f4f163
aamirmacma <54177613+aamirmacma@users.noreply.github.com>
2026-02-16 14:02:38 +0500

    Update Passport_Size_Maker.py

--- pages/Passport_Size_Maker.py
+++ pages/Passport_Size_Maker.py
@@ -1,35 +1,29 @@
 import streamlit as st
-from PIL import Image
-import io
+import cv2
+import numpy as np
 
-st.set_page_config(layout="wide")
+st.set_page_config(page_title="Passport Size Maker", layout="wide")
 
-st.title("🪪 Passport Size Maker")
+st.title("📐 Passport Size Maker")
 
-st.info("""
-NOTE:
-• JPG format only
-• Max size 500KB
-• Auto passport size adjustment
-""")
+st.info("JPG format | Max size 500KB")
 
-file = st.file_uploader("Upload Image", type=["jpg","jpeg"])
+file = st.file_uploader("Upload Photo", type=["jpg","jpeg"])
 
 if file:
 
-    img = Image.open(file)
+    file_bytes = np.asarray(bytearray(file.read()), dtype=np.uint8)
+    img = cv2.imdecode(file_bytes, 1)
 
-    # resize within range
-    img = img.resize((140,170))
+    resized = cv2.resize(img,(120,150))
 
-    st.image(img)
+    st.image(resized, channels="BGR")
 
-    buf = io.BytesIO()
-    img.save(buf, format="JPEG", quality=85)
+    _, buffer = cv2.imencode(".jpg", resized, [int(cv2.IMWRITE_JPEG_QUALITY),90])
 
     st.download_button(
         "Download Passport Size",
-        buf.getvalue(),
+        buffer.tobytes(),
         "passport_size.jpg",
         "image/jpeg"
     )
